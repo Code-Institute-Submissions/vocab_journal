89286ec7a786560c2b3b7e2a8046915ee282fc12
Damian Rodbari <skullphish@gmail.com>
2018-11-03 14:29:16 +0000

    method get_definitions of the OxDictApi class in py_define.py is fully cleaned up!

--- py_define.py
+++ py_define.py
@@ -1,153 +1,100 @@
 import requests
 import json
 
-# TODO: replace with your own app_id and app_key
+# --- API PREREQUISITES ---------------------------
 app_id = '413b2e3a'
 app_key = '84354fe4c72b838606558498c33f6eb1'
 language = 'en'
-
-# word_id = 'myriad'
-
-# # url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower() + '/synonyms;antonyms'
-# url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
-
-# r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
-
-# print("code {}\n".format(r.status_code))
-# # print("text \n" + r.text)
-# # print(r.text)
-# # print("json \n" + json.dumps(r.json()))
-# g = r.json()
-
-# results = g["results"][0]
-# print(type(results), results.keys())
-
-# # for key in results.keys():
-# # #   print(key, results[key])
-# #   print(key)
-# print('results["lexicalEntries"] type = ', type(results["lexicalEntries"]))
-# for key in results["lexicalEntries"]:
-#     print(key)
-# # print(results["lexicalEntries"][0].keys())
-
-# # for x in results["lexicalEntries"][0].keys():
-# #   print("x = ",x,results["lexicalEntries"][0][x])
-# # print(results["lexicalEntries"]['definitions'])
-# # print("final = ", type(final))
-
-
-# print("\n\n")
-# final = results["lexicalEntries"][0]["entries"][0]
-# # print(final["senses"][0])
-# final2 =  final["senses"][0]
-# print(final2["definitions"])
-# print(final2["short_definitions"])
-# print(final2["examples"])
 
 class OxDictApi:
     
     """ 
     do various task with dictionary api:
-    get definition of a word
-    get synonyms of a word
-    get example sentences for a word
+    get definition of a vocab
+    get synonyms of a vocab
+    get example sentences for a vocab
         
     """
     
-    def __init__(self, word):
+    def __init__(self, word, jdebug=0):
         self.word = word
+        self.jdebug = jdebug
         
-    def get_definition(self):
+    def get_definitions(self):
         """ get word definitions """
         
+        if self.jdebug > 0: print("\nget_definitions() method was called!")
+        
+        # --- PREREQUISITES ---------------------------
+        defs_output = dict() # blank dictionary to store all definintions in - function will return this dictionary once populated!
         url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + self.word.lower() + '/' + 'definitions'
         r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
-        print("code {}\n".format(r.status_code))
-        g = r.json()
-        # print(g)
-        # for k in g:
-        #     print(type(k), k)
         
+        # fetch response_code from API 
+        response_code = r.status_code
         
+        if self.jdebug > 0: print("response_code = {}".format(response_code))
         
-        # TIER 1 - get number of results!
-        # print(len(g["results"]) , type(g["results"]))
-        print("'{}' generated {} results in a list format".format(self.word, len(g["results"]) ) )
-        
-        # TIER 2 - get total number of lexicalEntries
-        print(len(g["results"][0]["lexicalEntries"]), type(g["results"][0]["lexicalEntries"]))
-        print("\tthere are a total of '{}' lexicalEntries in {} format ".format( len(g["results"][0]["lexicalEntries"]), type(g["results"][0]["lexicalEntries"]) ) )
-        
-        # contains list of dictionaries for each lexicalEntries ("Noun", "Adjective" and so on)
-        lexicalEntries = g["results"][0]["lexicalEntries"]
-        for lexicalEntry in lexicalEntries:
-            lexicalCategory = lexicalEntry["lexicalCategory"]
-            contents = lexicalEntry["entries"] # contains list of dicts [{"definition: []"},{"definition: []"}]
-            print(" ")
-            print("lexicalCategory = ", lexicalCategory)
-            print("contents = ", contents)
-            for content in contents:
-                senses = content["senses"]
-                for sense in senses:
-                    for vocab_def in sense["definitions"]:
-                        print("vocab_def = ", vocab_def)
-                try:
-                    subsenses = content["subsenses"]
-                    for subsense in subsenses:
-                        for vocab_def in subsense["definitions"]:
-                            print("vocab_def = ", vocab_def)
-                except:
-                    pass
+        # response_code "200": vocab was successfully found and picked up by the API
+        if response_code == 200:
+            g = r.json()
+        else:
+            # response_code "404": No entry is found matching supplied id and source_language. - BAIL!
+            # response_code "500": Internal Error. An error occurred while processing the data. - BAIL!
+            return response_code, defs_output
 
-        print("\n\n================================================")
-
-        
-        defs_output = dict()
-        results = g["results"]
+        # --- START ------------------------------------
+        results = g["results"] # get all the results to loop over one by one
         for resultNum in range(len(results)):
-            print("Analysing result number '{}' for '{}'".format(len(g["results"]), self.word ) )
+            if self.jdebug > 0: print("Analysing result number '{}' for '{}'".format(len(g["results"]), self.word ) )
             
             # contains list of dictionaries for each lexicalEntries ("Noun", "Adjective" and so on)
             lexicalEntries = results[resultNum]["lexicalEntries"]
-            for lexicalEntry in lexicalEntries:
-                lexicalCategory = lexicalEntry["lexicalCategory"]
-                lexicalEntryContent = lexicalEntry["entries"]
+            for lexicalEntry in lexicalEntries:                     # loop over all entries
+                lexicalCategory = lexicalEntry["lexicalCategory"]   # "Noun", "Adjective", "Adverb"
+                lexicalEntryContents = lexicalEntry["entries"]      # full content of "Noun" or "Adjective" or ...
                 
-                print("lexicalCategory = ", lexicalCategory) # "Noun", "Adjective"
-                # print("lexicalEntryContent = ", lexicalEntryContent) # content of "Noun" or "Adjective"
-                
-                contents = lexicalEntry["entries"] # contains list of dicts [{"definition: []"},{"definition: []"}]
-                def_list = []
-                for content in contents:
+                if self.jdebug > 2: print("\nlexicalCategory = {}".format(lexicalCategory))
+                if self.jdebug > 7: print("lexicalEntryContents = ", lexicalEntryContents) 
+
+                def_list = [] # collect all local definitions, senses or subsens(if exists)!
+                for content in lexicalEntryContents:
                     
+                    # fetch sense contents
                     senses = content["senses"]
                     for sense in senses:
                         for vocab_def in sense["definitions"]:
-                            print("vocab_def = ", vocab_def)
-                            def_list.append(vocab_def)
+                            def_list.append(vocab_def) # save vocab definition into "def_list"
                     try:
+                        # some vocabs dont have any subsenses
+                        # fetch sense subsenses
                         subsenses = content["subsenses"]
                         for subsense in subsenses:
                             for vocab_def in subsense["definitions"]:
-                                print("vocab_def = ", vocab_def)
-                                def_list.append(vocab_def)
+                                def_list.append(vocab_def) # save vocab definition into "def_list"
                     except:
-                        pass                
-                    print("def_list = ", def_list)
+                        pass
                     
+                    if self.jdebug > 2: print("def_list = ", def_list)
+                    
+                # store ALL extracted definitions based on their lexicalCategory(noun, adverb, ...)
                 defs_output[lexicalCategory] = def_list
                 
+        if self.jdebug > 4: print("\ndefs_output = {}\n".format(defs_output))  
+        
+        # NOTE: response_code will be "200" at this point
+        return response_code, defs_output
 
-        return defs_output
 
 
-
-# get_definition("myriad")
 
 myLocalDict = OxDictApi("quick")
+# myLocalDict = OxDictApi("kill", 5)
 # myLocalDict = OxDictApi("myriad")
 # myLocalDict = OxDictApi("hierarchy")
-kkobj = myLocalDict.get_definition()
+# myLocalDict = OxDictApi("kos")
+status , kkobj = myLocalDict.get_definitions()
 
+print("status = ", status)
 for k,v in kkobj.items():
         print("vocab as '{}' with definitions: {}".format(k,v))
