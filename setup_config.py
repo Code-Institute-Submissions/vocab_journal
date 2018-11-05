359385af68cce28a342f81b27dc714c510589a8c
Damian Rodbari <skullphish@gmail.com>
2018-11-05 21:08:55 +0000

    added a gitignore file and put setup_config.py in there

--- setup_config.py
+++
@@ -1,7 +0,0 @@
-# For mongoDB
-DB_NAME = 'vocabdb'
-DB_URI = 'mongodb://root:patriot1@ds223063.mlab.com:23063/vocabdb'
-
-# for oxford dictionary API
-APP_ID = '413b2e3a'
-APP_KEY = '84354fe4c72b838606558498c33f6eb1'
